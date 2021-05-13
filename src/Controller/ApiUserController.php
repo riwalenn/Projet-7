d4bdcac36d47fc70947f54ee1aee6461f10eb6d0
Riwalenn <riwalenn@gmail.com>
2021-05-13 16:38:38 +0200

    [MAJ] CRUD -> error message

--- src/Controller/ApiUserController.php
+++ src/Controller/ApiUserController.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
